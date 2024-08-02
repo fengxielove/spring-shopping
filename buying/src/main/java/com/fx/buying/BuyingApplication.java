5c4f069494c1690becb71144eb4bbdf72b927450
fengxie <1015806955@qq.com>
2024-08-02 17:30:14 +0800

    feat: init初始化项目

---
+++ buying/src/main/java/com/fx/buying/BuyingApplication.java
@@ -0,0 +1,13 @@
+package com.fx.buying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyingApplication.class, args);
+    }
+
+}
